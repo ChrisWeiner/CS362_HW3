fda4290e630ebe79da510e1dca4f98aaf57b7b6b
Christopher Weiner <chriscweiner@gmail.com>
2021-01-28 15:40:38 -0800

    Addition of Python files (with and without input validation), along with flow charts to add into README.md

---
+++ CW_LeapYear_Check.py
@@ -0,0 +1,27 @@
+#Christopher Weiner
+#CS 362
+#Assignment 3
+#Leap Year - Input Checking/Validation
+
+def leapYearCheck(x):
+    if(x % 4) == 0:
+        if(x % 100) == 0:
+            if(x % 400) == 0:
+                print(str(x) + " is a leap year")
+                return
+            else:
+                print(str(x) + " is not a leap year")
+                return
+        else:
+            print(str(x) + " is a leap year")
+            return
+    else:
+        print(str(x) + " is not a leap year")
+        return
+
+print("Please enter a number to see if it is a leap year:")
+x = input()
+if x.isdigit() == False:
+    print(str(x) + " is not a number")
+    exit()
+else: leapYearCheck(int(x))
